1db3360b97a9cd3895f050fbcd92782f96adb4ae
wangmeng <wangmeng@haoyuanyiliao.com>
2020-05-14 20:27:35 +0800

    添加多命令执行

--- ci/startup.sh
+++ ci/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /app
 source /etc/profile
-nohup frontail ./stdout.log
+nohup frontail ./stdout.log 2>&1 &
 nohup java -jar -Dfile.encoding=utf-8 -Duser.timezone=GMT+8 /app/app.jar --spring.profiles.active=$APP_ENV --codingfly.swagger.version=$IMAGE_TAG >> stdout.log 2>&1 &
 nohup tail -f stdout.log >> /var/log/stdout.log 2>&1 &
 exec "$@"
