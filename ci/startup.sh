d99e2d6f184f0cd1fffd833a4b464955b467cd75
wangmeng <wangmeng@haoyuanyiliao.com>
2020-05-15 09:37:59 +0800

    恢复原版本

--- ci/startup.sh
+++ ci/startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /app
 source /etc/profile
-touch stdout.log
-nohup frontail ./stdout.log 2>&1
-sleep 30s 2>&1
 nohup java -jar -Dfile.encoding=utf-8 -Duser.timezone=GMT+8 /app/app.jar --spring.profiles.active=$APP_ENV --codingfly.swagger.version=$IMAGE_TAG >> stdout.log 2>&1 &
-nohup tail -f stdout.log >> /var/log/stdout.log
+nohup tail -f stdout.log >> /var/log/stdout.log 2>&1 &
+nohup frontail ./stdout.log
 exec "$@"
