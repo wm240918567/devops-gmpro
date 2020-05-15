96cc00ffdf69441ac071246f8deac1b4ecece13e
wangmeng <wangmeng@haoyuanyiliao.com>
2020-05-15 09:14:02 +0800

    尝试frontail改造

--- ci/startup.sh
+++ ci/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /app
 source /etc/profile
-nohup frontail ./stdout.log 2>&1 &
-sleep 10s 2>&1 &
+nohup frontail ./stdout.log 2>&1
+sleep 30s 2>&1
 nohup java -jar -Dfile.encoding=utf-8 -Duser.timezone=GMT+8 /app/app.jar --spring.profiles.active=$APP_ENV --codingfly.swagger.version=$IMAGE_TAG >> stdout.log 2>&1 &
 nohup tail -f stdout.log >> /var/log/stdout.log
 exec "$@"
